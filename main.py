31c2350d831d5095164058e14dbbaf779a03fa28
Sofiaa-dotcom <72768448+Sofiaa-dotcom@users.noreply.github.com>
2020-11-19 14:53:49 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-tempf= 70
+tempf= 75
 while True:
     print("temperature" + input.temperature(TemperatureUnit.FAHRENHEIT))
     if input.temperature(TemperatureUnit.FAHRENHEIT) > tempf:
         light.set_pixel_color(5, light.rgb(255,0,0))
     elif tempf < 70:
-    if input.temperature(TemperatureUnit.FAHRENHEIT) < tempf:
-    light.set_pixel_color(6, light.rgb(0,128,0))
+     if input.temperature(TemperatureUnit.FAHRENHEIT) < tempf:
+         light.set_pixel_color(6, light.rgb(0,128,0))
 else:
     light.clear()
--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-let tempf = 70
-while (true) {
-    console.log("temperature" + input.temperature(TemperatureUnit.Fahrenheit))
-    if (input.temperature(TemperatureUnit.Fahrenheit) > tempf) {
-        light.setPixelColor(5, light.rgb(255, 0, 0))
-    } else {
-        light.clear()
-    }
-    
-}
+
